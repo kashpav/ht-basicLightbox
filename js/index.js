b2fd4858712836ea61549d2b9c7555177d705a19
Pavlo Kashchenko <pavlo.kashchenko@gmail.com>
2022-08-15 17:45:02 +0300

    варіанти лайтбоксів

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <img src="assets/images/image.png" width="800" height="600">
+//     </div>
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(`
+//     <video controls>
+//         <source src="assets/videos/video.mp4" type="video/mp4">
+//     </video>
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(`
+//     <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
+// `);
+
+// instance.show();
+
+// const instance = basicLightbox.create(
+//   `
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `,
+//   {
+//     onShow: instance => {
+//       instance.element().querySelector('a').onclick = instance.close;
+//     },
+//   }
+// );
+
+// instance.show();
